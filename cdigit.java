f08d99f7edfd46b41299f86f0e54f8ce8bb3d43e
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-27 15:50:59 +0530

    Create cdigit.java

---
+++ cdigit.java
@@ -0,0 +1,13 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=6786,c=0;
+		while(n!=0)
+		{
+			n=n/10;
+			c++;
+		}
+		System.out.print(c);
+	}
+}
