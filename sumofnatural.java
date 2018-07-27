229a87700b57553a6f098ee0dd0df1a91337239c
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-27 15:58:20 +0530

    Create sumofnatural.java

---
+++ sumofnatural.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int k=40,sum=0;
+		for(int i=1;i<=k;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.print(sum);
+	}
+}
