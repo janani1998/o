16932e8dcd132603f97e13626802044df64c2433
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-27 14:55:00 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=321;
+		if(n%2==0)
+			System.out.print(n);
+		else
+		System.out.print(n-1);
+	}
+}
