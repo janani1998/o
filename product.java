9830b88c8fe8273e8b9efecfd2840677ed197f90
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-27 15:00:17 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=21,b=1;
+		if((a*b)%2==0)
+		System.out.print("Even");
+		else
+		System.out.print("Odd");
+	}
+}
