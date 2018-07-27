f3687cb39c48ef2904c2d5c377ab8b400b09ed39
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-27 14:40:58 +0530

    Create word.java

---
+++ word.java
@@ -0,0 +1,9 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=1;
+		String s[]={"zero","one","two","three","four","five","six","seven","eight","nine","ten"};
+		System.out.print(s[n]);
+	}
+}
