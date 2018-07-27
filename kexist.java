fb0ffebaa72b2d17c829e24da0cda76adea6f74b
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-27 15:47:09 +0530

    Create kexist.java

---
+++ kexist.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class kcount {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int k=in.nextInt();
+		int a[]=new int[n];
+		int i,j,c=0;
+		for(i=0;i<n;i++)
+		{
+			a[i]=in.nextInt();
+				if(a[i]==k)
+					c++;
+			}
+		//System.out.println(c);
+		if(c!=0)
+			System.out.print("Yes");
+		else
+			System.out.print("No");
+	}
+}
